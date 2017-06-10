3db4cb9a07de27390d4ee48507d2c129fdc06bba
45deg <noreply>
2017-06-10 17:50:22 +0900

    Removed an unused file

--- project/Build.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-object MyBuild extends Build {
-  lazy val root = project.in(file(".")).dependsOn(githubRepo)
-  lazy val githubRepo = uri("git://github.com/45deg/Actoverse-Scala.git")
-}
